f69186b34592dc68a2375f593962a6c84c5b5c64
Luiz Fernando Mattos <luizfernandoamattos@gmail.com>
2022-11-14 08:07:33 -0300

    kick inactive users

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import Joi from "joi";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import participantSchema from "./schemas/participantSchema.js";
 import messageSchema from "./schemas/messageSchema.js";
 import dayjs from "dayjs";
@@ -14,27 +14,37 @@ app.use(express.json());
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 await mongoClient.connect();
-const db = mongoClient.db("chatUol");
-const participants = db.collection("participants");
-const messages = db.collection("messages");
+const participants = mongoClient.db("chatUol").collection("participants");
+const messages = mongoClient.db("chatUol").collection("messages");
 
-function verifyNameOccurrence(names, userName) {
-  for (let name of names) {
-    if (
-      name
-        .toLowerCase()
-        .normalize("NFD")
-        .replace(/[^a-zA-Z\s]/g, "") ===
-      userName
-        .toLowerCase()
-        .normalize("NFD")
-        .replace(/[^a-zA-Z\s]/g, "")
-    ) {
-      return true;
+setInterval(async () => {
+  try {
+    const disconnectedParticipants = await participants
+      .find({
+        lastStatus: { $lt: Date.now() - 10000 },
+      })
+      .toArray();
+
+    if (disconnectedParticipants.length !== 0) {
+      disconnectedParticipants.forEach(
+        async (p) =>
+          await messages.insertOne({
+            from: p.name,
+            to: "Todos",
+            text: "sai da sala...",
+            type: "status",
+            time: dayjs().format("hh:mm:ss"),
+          })
+      );
+      await participants.deleteMany({
+        lastStatus: { $lt: Date.now() - 10000 },
+      });
     }
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
   }
-  return false;
-}
+}, 60000);
 
 app.post("/participants", async (req, res) => {
   const { name } = req.body;
@@ -45,26 +55,24 @@ app.post("/participants", async (req, res) => {
     return;
   }
 
-  const allNames = await participants.distinct("name");
-  const isNameExist = verifyNameOccurrence(allNames, name);
-  if (isNameExist) {
-    res.status(409).send({ message: "Usuário já existente" });
-    return;
-  }
-
   try {
+    const user = await participants.findOne({ name });
+    if (user) {
+      res.status(409).send({ message: "Usuário já existente" });
+      return;
+    }
     await participants.insertOne({ name, lastStatus: Date.now() });
-    messages.insertOne({
+    await messages.insertOne({
       from: name,
       to: "Todos",
       text: "entra na sala...",
       type: "status",
       time: dayjs().format("hh:mm:ss"),
     });
-    res.send(201);
+    return res.sendStatus(201);
   } catch (err) {
     console.log(err);
-    res.sendStatus(500);
+    return res.sendStatus(500);
   }
 });
 
@@ -79,8 +87,8 @@ app.get("/participants", async (req, res) => {
 
 app.post("/messages", async (req, res) => {
   const { to, text, type } = req.body;
-  const { user } = req.headers;
-  const message = { from: user, to, text, type };
+  const { user: username } = req.headers;
+  const message = { from: username, to, text, type };
   const validation = messageSchema.validate(message, { abortEarly: false });
 
   if (validation.error) {
@@ -89,7 +97,12 @@ app.post("/messages", async (req, res) => {
   }
 
   try {
-    await messages.insertOne({ from, to, text, type });
+    const user = await participants.findOne({ name: username });
+    if (!user) {
+      res.status(422).send({ message: "O usuário não está logado" });
+      return;
+    }
+    await messages.insertOne(message);
     res.send({ message: "Mensagem enviada." });
   } catch (err) {
     console.log(err);
@@ -100,7 +113,24 @@ app.post("/messages", async (req, res) => {
 app.get("/messages", async (req, res) => {
   try {
     res.send(await messages.find().toArray());
-    return;
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+app.post("/status", async (req, res) => {
+  const { user: username } = req.headers;
+  try {
+    const user = await participants.findOne({ name: username });
+    if (!user) {
+      return res.sendStatus(404);
+    }
+    await participants.updateOne(
+      { name: username },
+      { $set: { name: username, lastStatus: Date.now() } }
+    );
+    res.status(200);
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
--- src/schemas/messageSchema.js
+++ src/schemas/messageSchema.js
@@ -1,10 +1,11 @@
+import e from "express";
 import joi from "joi";
 
 const messageSchema = joi.object({
   from: joi.string().required(),
   to: joi.string().required(),
   text: joi.string().required(),
-  type: joi.string().required(),
+  type: joi.string().valid("message", "private_message").required(),
 });
 
 export default messageSchema;
